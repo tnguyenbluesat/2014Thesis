0ed56b703cef295c1f2d7ac5e1a5596e3413b9e0
Thien Nguyen <tnguyen@bluesat.unsw.edu.au>
2014-03-13 11:42:17 +1100

    added all model files

---
+++ STK Models/US_Greatcircle_invMolniya/Chain3.c
@@ -0,0 +1,100 @@
+stk.v.10.0
+WrittenBy    STK_v10.0.2
+
+BEGIN Chain
+
+Name  Chain3
+BEGIN Definition
+
+   Type        Chain
+   Operator    Or
+   Order       1
+   Recompute   Yes
+   IntervalType    0
+   ComputeIntervalStart    0.000000
+   ComputeIntervalStop     7300800.000000
+    ComputeIntervalPtr
+	BEGIN	EVENTINTERVAL
+			BEGIN Interval
+				Start	3 Dec 2013 01:00:00.000000000
+				Stop	25 Feb 2014 13:00:00.000000000
+			END Interval
+			IntervalState	Explicit
+	END	EVENTINTERVAL
+
+   UseSaveIntervalFile    No
+   UseMinAngle     No
+   UseMaxAngle     No
+   UseMinLinkTime     No
+   LTDelayCriterion    2.000000
+   TimeConvergence     0.005000
+   AbsValueConvergence 1.000000e-014
+   RelValueConvergence 1.000000e-008
+   MaxTimeStep         360.000000
+   MinTimeStep         1.000000e-002
+   UseLightTimeDelay   Yes
+    DetectEventsUsingSamplesOnly No
+    Object  Aircraft/B747LAX_Heathrow/Transmitter/adsb_out6
+    Object  Facility/Heathrow/Receiver/ADSB_GndRcvr11
+   SaveMode    1
+BEGIN StrandAccesses
+
+  Strand    Aircraft/B747LAX_Heathrow/Transmitter/adsb_out6 To Facility/Heathrow/Receiver/ADSB_GndRcvr11
+    Start    0
+    Stop     1536.0948844327204
+    Start    68643.275193177164
+    Stop     71715.46201485263
+    Start    138822.64232360033
+    Stop     141894.82914525922
+    Start    209002.00945486856
+    Stop     212074.19627569869
+    Start    279181.3765843669
+    Stop     282253.56340612075
+    Start    349360.74371571286
+    Stop     352432.93053654279
+    Start    419540.11084605614
+    Stop     421076.20278253598
+END StrandAccesses
+
+   UseLoadIntervalFile    No
+
+END Definition
+
+BEGIN Extensions
+    
+    BEGIN Graphics
+
+BEGIN Attributes
+
+StaticColor					#ff00ff
+AnimationColor					#ffff00
+AnimationLineWidth					2.000000
+StaticLineWidth					3.000000
+
+END Attributes
+
+BEGIN Graphics
+    ShowGfx                Off
+    ShowStatic             Off
+    ShowAnimationHighlight On
+    ShowAnimationLine      On
+    ShowLinkDirection      Off
+END Graphics
+    END Graphics
+    
+    BEGIN ADFFileData
+    END ADFFileData
+    
+    BEGIN Desc
+    END Desc
+    
+    BEGIN Crdn
+    END Crdn
+    
+    BEGIN VO
+    END VO
+
+END Extensions
+
+END Chain
+
